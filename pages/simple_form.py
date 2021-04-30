d6e35b530b2f7cedbc6dcb9b8abdc21e6323347c
lemy12 <bae9lomi@gmail.com>
2021-04-30 16:50:18 +0200

    two input tests

--- pages/simple_form.py
+++ pages/simple_form.py
@@ -1,6 +1,5 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.common.keys import Keys
 
 
 class DemoSimpleFormPage:
@@ -10,6 +9,11 @@ class DemoSimpleFormPage:
     SINGLE_INPUT_SHOW = (By.XPATH, '/html/body/div[2]/div/div[2]/div[1]/div[2]/form/button')
     SINGLE_INPUT_MESSAGE = (By.ID, 'display')
 
+    TWO_INPUT_TEXTBOX_A = (By.ID, 'sum1')
+    TWO_INPUT_TEXTBOX_B = (By.ID, 'sum2')
+    TWO_INPUT_GET = (By.XPATH, '/html/body/div[2]/div/div[2]/div[2]/div[2]/form/button')
+    TWO_INPUT_TOTAL = (By.ID, 'displayvalue')
+
     def __init__(self, browser):
         self.browser = browser
 
@@ -18,7 +22,20 @@ class DemoSimpleFormPage:
 
     def check_single_input(self, phrase):
         textbox = self.browser.find_element(*self.SINGLE_INPUT_TEXTBOX)
-        textbox.send_keys(phrase)
         show = self.browser.find_element(*self.SINGLE_INPUT_SHOW)
+        ActionChains(self.browser).move_to_element(textbox).double_click().perform()
+        textbox.send_keys(phrase)
         ActionChains(self.browser).move_to_element(show).click().perform()
         return self.browser.find_element(*self.SINGLE_INPUT_MESSAGE).text
+
+    def placeholder_message(self):
+        return self.browser.find_element(*self.SINGLE_INPUT_TEXTBOX).get_attribute("placeholder")
+
+    def check_two_input(self, a, b):
+        textbox_a = self.browser.find_element(*self.TWO_INPUT_TEXTBOX_A)
+        textbox_b = self.browser.find_element(*self.TWO_INPUT_TEXTBOX_B)
+        get = self.browser.find_element(*self.TWO_INPUT_GET)
+        textbox_a.send_keys(a)
+        textbox_b.send_keys(b)
+        ActionChains(self.browser).click(get).perform()
+        return self.browser.find_element(*self.TWO_INPUT_TOTAL).text
--- tests/test_simple_form.py
+++ tests/test_simple_form.py
@@ -2,13 +2,35 @@ import pytest
 
 from pages.simple_form import DemoSimpleFormPage
 
+PHRASE = ['text']
+NUMBER = [('1', '2', '3'), ('-1', '2', '1')]
 
-@pytest.mark.parametrize('phrase', ['yes', 'no', '123', ''])
+
+@pytest.mark.single_input
+@pytest.mark.parametrize('phrase', PHRASE)
 def test_single_input(browser, phrase):
     page = DemoSimpleFormPage(browser)
 
+    # Load webpage
     page.load()
 
-    result = page.check_single_input(phrase)
+    # Assert that form shows written phrase
+    assert page.check_single_input(phrase) == phrase
 
-    assert phrase == result
+    # Assert that form changes to new message
+    assert page.check_single_input("message") == "message"
+
+    # Assert that textbox has placeholder message
+    assert page.placeholder_message() and page.placeholder_message() != ""
+
+
+@pytest.mark.two_input
+@pytest.mark.parametrize('a,b,expected', NUMBER)
+def test_single_input(browser, a, b, expected):
+    page = DemoSimpleFormPage(browser)
+
+    # Load webpage
+    page.load()
+
+    # Assert that total show correct sum
+    assert page.check_two_input(a, b) == expected
