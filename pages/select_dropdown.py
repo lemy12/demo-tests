12fac2dfa0333e76413330ce3a2bd73d4a0b89bd
lemy12 <bae9lomi@gmail.com>
2021-05-09 21:46:16 +0200

    multi select dropdown tests

--- pages/select_dropdown.py
+++ pages/select_dropdown.py
@@ -1,5 +1,6 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
 
 
@@ -8,13 +9,12 @@ class DemoSelectDropdownPage:
 
     # Select list variables
     SELECT_LIST_DROPDOWN = (By.ID, 'select-demo')
-    SELECT_LIST_DROPDOWN_OPTIONS = (By.XPATH, '/html/body/div[2]/div/div[2]/div[1]/div[2]/select/option')
     SELECT_LIST_RESULT = (By.CLASS_NAME, 'selected-value')
 
     TEMP = (By.XPATH, '/html/body/div[2]/div/div[2]/div[1]/div[2]/select/option[3]')
 
     # Multi select list variables
-    MSELECT_LIST_OPTIONS = (By.XPATH, '/html/body/div[2]/div/div[2]/div[2]/div[2]/select/option')
+    MSELECT_LIST_MAIN = (By.ID, 'multi-select')
     MSELECT_LIST_BUTTON_FIRST = (By.ID, 'printMe')
     MSELECT_LIST_BUTTON_ALL = (By.ID, 'printAll')
     MSELECT_LIST_RESULT = (By.CLASS_NAME, 'getall-selected')
@@ -30,5 +30,26 @@ class DemoSelectDropdownPage:
         result = self.browser.find_element(*self.SELECT_LIST_RESULT)
 
         dropdown.select_by_value(day)
+
+        return result.text
+
+    def check_multi_select_list(self, city, choice):
+        dropdown = self.browser.find_element(*self.MSELECT_LIST_MAIN)
+        dropdown_options = dropdown.find_elements_by_tag_name("option")
+        result_first = self.browser.find_element(*self.MSELECT_LIST_BUTTON_FIRST)
+        result_all = self.browser.find_element(*self.MSELECT_LIST_BUTTON_ALL)
+        result = self.browser.find_element(*self.MSELECT_LIST_RESULT)
+
+        action_builder = ActionChains(self.browser).move_to_element(dropdown).key_down(Keys.CONTROL)
+        for name in city:
+            for option in dropdown_options:
+                if option.text == name:
+                    action_builder.move_to_element(option).click()
+        action_builder.perform()
+
+        if choice == "first":
+            ActionChains(self.browser).move_to_element(result_first).click().perform()
+        elif choice == "all":
+            ActionChains(self.browser).move_to_element(result_all).click().perform()
 
         return result.text
--- tests/test_select_dropdown.py
+++ tests/test_select_dropdown.py
@@ -4,11 +4,14 @@ from pages.select_dropdown import DemoSelectDropdownPage
 
 # Parametrize globals
 DAY = ['Sunday', 'Saturday']
+CITY = [(['California'], 'first'),
+        (['Pennsylvania', 'Washington'], 'first'),
+        (['Florida', 'Ohio', 'Texas'], 'all')]
 
 
-@pytest.mark.select_dropdown
+@pytest.mark.select_list
 @pytest.mark.parametrize('day', DAY)
-def test_radio_button(browser, day):
+def test_select_list(browser, day):
     page = DemoSelectDropdownPage(browser)
 
     # Load webpage
@@ -18,3 +21,21 @@ def test_radio_button(browser, day):
     assert day in page.check_select_list(day)
 
 
+@pytest.mark.multi_select_list
+@pytest.mark.parametrize('city,choice', CITY)
+def test_multi_select_list(browser, city, choice):
+    page = DemoSelectDropdownPage(browser)
+
+    # Load webpage
+    page.load()
+
+    # Assert that chosen day is in result message
+    result = page.check_multi_select_list(city, choice)
+
+    if choice == "first":
+        assert "First selected option is :" in result
+        assert city[0] in result
+    elif choice == "all":
+        assert "Options selected are :" in result
+        for each in city:
+            assert each in result
